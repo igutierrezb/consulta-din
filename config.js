c38be5e9d0c9236e142e10f06143044fd7fcae8a
igutierrezb <70083353+igutierrezb@users.noreply.github.com>
2026-09-17 13:23:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DIN_CONFIG = {
-  sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtwFswP5R6Vgu-WIo',
+  sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtWfswP5R6Vgu-WIo',
   endpoint: 'https://script.google.com/macros/s/AKfycbw8p0g6E7q3gZpR6FoYrbHhcbv6Nk3IoPCE031WTeBlJzYxTyjZsMBFE5prsG2jdvfP/exec',
   contacto: 'ivan.gutierrez@uteq.edu.mx',
   extension: '2441'
