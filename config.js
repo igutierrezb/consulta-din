56a42d26a08bb5a2d2100e83884e5352d58ea42e
igutierrezb <70083353+igutierrezb@users.noreply.github.com>
2026-09-17 11:16:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-/* Configuración pública. Los dos IDs de Drive se editan SOLO en administracion/Config.gs. */
 window.DIN_CONFIG = {
- sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtWfswP5R6Vgu-WIo',
- endpoint: '', // Pegar aquí https://script.google.com/macros/s/.../exec
- contacto: 'ivan.gutierrez@uteq.edu.mx', extension: '2441'
+  sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtwFswP5R6Vgu-WIo',
+  endpoint: 'https://script.google.com/macros/s/AKfycbw8p0g6E7q3gZpR6FoYrbHhcbv6Nk3IoPCE031WTeBlJzYxTyjZsMBFE5prsG2jdvfP/exec',
+  contacto: 'ivan.gutierrez@uteq.edu.mx',
+  extension: '2441'
 };
