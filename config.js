2b5f3fa120273ec50f606085a132659750511cc0
igutierrezb <70083353+igutierrezb@users.noreply.github.com>
2026-09-18 06:39:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 window.DIN_CONFIG = {
+  adminUrl: 'https://script.google.com/a/macros/uteq.edu.mx/s/AKfycbwc7vC-K88yU7DQqRYDORHpGJfm7NHtvSBkT6s_KzC6zSAtvTewcifdFQ4__VR2iW8/exec',
   sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtWfswP5R6Vgu-WIo',
   endpoint: 'https://script.google.com/macros/s/AKfycbw8p0g6E7q3gZpR6FoYrbHhcbv6Nk3IoPCE031WTeBlJzYxTyjZsMBFE5prsG2jdvfP/exec',
   contacto: 'ivan.gutierrez@uteq.edu.mx',
   extension: '2441'
 };
+
