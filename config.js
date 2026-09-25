04cc554d3cce1d4d3ce1bf53e6dd894258456988
igutierrezb <70083353+igutierrezb@users.noreply.github.com>
2026-09-25 15:56:25 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DIN_CONFIG = {
-  build: '2026-09-25-final-r7',
+  build: '2026-09-25-final-r8',
   adminUrl: 'https://script.google.com/a/macros/uteq.edu.mx/s/AKfycbwc7vC-K88yU7DQqRYDORHpGJfm7NHtvSBkT6s_KzC6zSAtvTewcifdFQ4__VR2iW8/exec',
   sheetId: '1LmAg9YJesg79BmDfi7Iax0eL27jtWfswP5R6Vgu-WIo',
   endpoint: 'https://script.google.com/macros/s/AKfycbw8p0g6E7q3gZpR6FoYrbHhcbv6Nk3IoPCE031WTeBlJzYxTyjZsMBFE5prsG2jdvfP/exec',
